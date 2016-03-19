be3bd67c710aff0f4cb219577ac3e955ae5f0877
orichalque <thiblf@gmail.com>
2016-03-19 16:44:04 +0100

    cleanning the repo

--- quelleEstCetteNote/QuelleEstCetteNote2/portee.cpp
+++ quelleEstCetteNote/QuelleEstCetteNote2/portee.cpp
@@ -18,6 +18,10 @@ Portee::Portee(QWidget *parent) :
     setFixedSize(780, 490);
 }
 
+void Portee::setPiano(Piano* p){
+    piano = p;
+}
+
 void Portee::paintEvent(QPaintEvent *event){
     QWidget::paintEvent(event);
     QPainter p(this);
@@ -36,7 +40,7 @@ void Portee::paintEvent(QPaintEvent *event){
 
     // 0-14 note -> 1 line
     // 14 - 28 -> 2 line
-
+   // int nbOfNotesPerLine = 14;
     int nbOfLine = int (noteAmount / 15 +1);
 
     float interLineSpace = h*(0.07/nbOfLine);
@@ -69,6 +73,16 @@ void Portee::paintEvent(QPaintEvent *event){
     p.drawImage(0.15*w, 0.035*h / nbOfLine + upSpace, key);
 
 
+    QImage blackN(":/new/prefix1/black.png"); //Note normale
+    QImage blackD(":/new/prefix1/black_downLine.png"); //Note avec barre en bas sim, reM
+    QImage blackU(":/new/prefix1/black_upLine.png"); //Note avec barre en haut : solm,
+    QImage blackM(":/new/prefix1/black_middleLine.png"); //Note avec barre au milieu : lam, doM
+    QImage blackDiese(":/new/prefix1/black_diese.png"); //Note diese
+    blackN = blackN.scaled(0.05*w, 6.5*interLineSpace);
+    blackD = blackD.scaled(0.05*w, 6.5*interLineSpace);
+    blackU = blackU.scaled(0.05*w, 6.5*interLineSpace);
+    blackM = blackM.scaled(0.05*w, 6.5*interLineSpace);
+    blackDiese = blackDiese.scaled(0.05*w, 6.5*interLineSpace);
 
     //Notes
     // interLineSpace hauteur de la note
@@ -76,69 +90,148 @@ void Portee::paintEvent(QPaintEvent *event){
     //noteWidth a definir en fonction de la largeur de l'ecran
 
     qreal xNote = 0.22*w;
-    qreal yNote = (upSpace + 0.207*h);
+    qreal yNote;
 
-    QRectF *rect;
-    QLine *line;
     note *n;
     //(xNote,yNote , w*0.025 ,interLineSpace)
     vector<QRectF *> rectToDraw;
     int nbOfNotesDrawn;
+    bool up, down, mid, diese;
     for (auto noteName : notes){
-
-        yNote = (upSpace + 0.207*h + ((2*upSpace + 5*interLineSpace )*(nbOfNotesDrawn/14))); //Modulo le nombre de portées ! //TODO
+        up = down = mid = diese = false;
+        yNote = 0.118*h + (((0.275*h)*(floor(nbOfNotesDrawn/14)))); //Modulo le nombre de portées ! //TODO
+        cout << yNote << endl;
         if (nbOfNotesDrawn % 14 == 0 && nbOfNotesDrawn > 0){
             xNote = 0.22*w;
         }
-
-        if (noteName == "cm" || noteName =="dom"){
+        //if (this->piano->note_)
+        p.drawLine(xNote, yNote, xNote , yNote + 5*interLineSpace);
+        /* NOTE CLASSIQUE */
+        if (noteName == "cm" || noteName =="doM"){
             //yNote immobile
-        }
-        if (noteName == "dm" || noteName =="rem"){
+            mid = true;
+        } else
+        if (noteName == "dm" || noteName =="reM"){
             yNote = yNote - 0.5*interLineSpace;
-        }
-        if (noteName == "em" || noteName =="mim"){
+            down = true;
+        } else
+        if (noteName == "em" || noteName =="miM"){
             yNote = yNote - 1*interLineSpace;
-        }
-        if (noteName == "fm" || noteName =="fam"){
+        } else
+        if (noteName == "fm" || noteName =="faM"){
             yNote = yNote - 1.5*interLineSpace;
-        }
-        if (noteName == "gm" || noteName =="solm"){
+        } else
+        if (noteName == "gm" || noteName =="solM"){
             yNote = yNote - 2*interLineSpace;
-        }
-        if (noteName == "am" || noteName =="lam"){
+        } else
+        if (noteName == "am" || noteName =="laM"){
             yNote = yNote - 2.5*interLineSpace;
-        }
-        if (noteName == "bm" || noteName =="sim"){
+        } else
+        if (noteName == "bm" || noteName =="siM"){
             yNote = yNote - 3*interLineSpace;
+        } else
+        if (noteName == "cM" || noteName =="dom"){
+            yNote = yNote - 3.5*interLineSpace;
+        } else
+        if (noteName == "dM" || noteName =="rem"){
+            yNote = yNote - 4*interLineSpace;
+        } else
+        if (noteName == "eM" || noteName =="mim"){
+            yNote = yNote - 4.5*interLineSpace;
+        } else
+        if (noteName == "fM" || noteName =="fam"){
+            yNote = yNote - 5*interLineSpace;
+        } else
+        if (noteName == "gM" || noteName =="solm"){
+            yNote = yNote - 5.5*interLineSpace;
+            up = true;
+        } else
+        if (noteName == "aM" || noteName =="lam"){
+            yNote = yNote - 6*interLineSpace;
+            mid = true;
+        } else
+        if (noteName == "bM" || noteName =="sim"){
+            yNote = yNote - 6.5*interLineSpace;
+            down = true;
+        } else
+
+        /* NOTE DIESE */
+        if (noteName == "cm" || noteName =="doMD"){
+            //yNote immobile
+            mid = true;
+        } else
+        if (noteName == "dm" || noteName =="reMD"){
+            yNote = yNote - 0.5*interLineSpace;
+            down = true;
+            diese = true;
+        } else
+        if (noteName == "em" || noteName =="miMD"){
+            yNote = yNote - 1*interLineSpace;
+            diese = true;
+        } else
+        if (noteName == "fm" || noteName =="faMD"){
+            yNote = yNote - 1.5*interLineSpace;
+            diese = true;
+        } else
+        if (noteName == "gm" || noteName =="solMD"){
+            yNote = yNote - 2*interLineSpace;
+            diese = true;
+        } else
+        if (noteName == "am" || noteName =="laMD"){
+            yNote = yNote - 2.5*interLineSpace;
+            diese = true;
+        } else
+        if (noteName == "bm" || noteName =="siMD"){
+            yNote = yNote - 3*interLineSpace;
+            diese = true;
+        } else
+        if (noteName == "cM" || noteName =="domD"){
+            yNote = yNote - 3.5*interLineSpace;
+            diese = true;
+        } else
+        if (noteName == "dM" || noteName =="remD"){
+            yNote = yNote - 4*interLineSpace;
+            diese = true;
+        } else
+        if (noteName == "eM" || noteName =="mimD"){
+            yNote = yNote - 4.5*interLineSpace;
+            diese = true;
+        } else
+        if (noteName == "fM" || noteName =="famD"){
+            yNote = yNote - 5*interLineSpace;
+            diese = true;
+        } else
+        if (noteName == "gM" || noteName =="solmD"){
+            yNote = yNote - 5.5*interLineSpace;
+            up = true;
+            diese = true;
+        } else
+        if (noteName == "aM" || noteName =="lamD"){
+            yNote = yNote - 6*interLineSpace;
+            mid = true;
+            diese = true;
+        } else
+        if (noteName == "bM" || noteName =="simD"){
+            yNote = yNote - 6.5*interLineSpace;
+            down = true;
+            diese = true;
         }
 
-        if (noteName == "cM" || noteName =="doM"){
-            yNote = yNote - 3.5*interLineSpace;
+        if (down == true){
+            p.drawImage(xNote, yNote, blackD);
+        } else if (up == true) {
+            p.drawImage(xNote, yNote, blackU);
+        } else if (mid == true) {
+            p.drawImage(xNote, yNote, blackM);
+        } else {
+            p.drawImage(xNote, yNote, blackN);
         }
-        if (noteName == "dM" || noteName =="reM"){
-            yNote = yNote - 4*interLineSpace;
+
+        if (diese) {
+            p.drawImage(xNote, yNote, blackDiese);
         }
-        if (noteName == "eM" || noteName =="miM"){
-            yNote = yNote - 4.5*interLineSpace;
-        }
-        if (noteName == "fM" || noteName =="faM"){
-            yNote = yNote - 5*interLineSpace;
-        }
-        if (noteName == "gM" || noteName =="solM"){
-            yNote = yNote - 5.5*interLineSpace;
-        }
-        if (noteName == "aM" || noteName =="laM"){
-            yNote = yNote - 6*interLineSpace;
-        }
-        if (noteName == "bM" || noteName =="siM"){
-            yNote = yNote - 6.5*interLineSpace;
-        }
-        rect = new QRectF(xNote,yNote , w*0.025 ,interLineSpace);
-        line = new QLine(xNote + 0.026*w, yNote+0.5*interLineSpace, xNote + 0.026*w, yNote-3*interLineSpace);
-        n = new note(noteName, *rect, *line);
-        notesToPlay.push_back(*n);
-        n -> draw(&p);
+
+
 
         xNote = xNote + 0.045*w;
 
--- quelleEstCetteNote/QuelleEstCetteNote2/portee.h
+++ quelleEstCetteNote/QuelleEstCetteNote2/portee.h
@@ -2,15 +2,18 @@
 #define PORTEE_H
 #include <QWidget>
 #include "notereader.h"
+#include "piano.h"
 
 class Portee : public QWidget
 {
 public:
     Portee();
     explicit Portee(QWidget *parent = 0);
+    void setPiano(Piano* p);
+
 protected:
     void paintEvent(QPaintEvent * event);
-
+    Piano* piano;
 };
 
 #endif // PORTEE_H
