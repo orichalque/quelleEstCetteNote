c38d6b0fe9561d41bda1b09a0a787b26824378b7
dennisman <dennisman@laposte.net>
2016-03-21 19:35:31 +0100

    .h commenté pour dennis

--- quelleEstCetteNote/QuelleEstCetteNote2/piano.h
+++ quelleEstCetteNote/QuelleEstCetteNote2/piano.h
@@ -1,3 +1,9 @@
+/**
+ * \file piano.h
+ * \brief contient la Classe Piano hérité de QWidget.
+ * Comprend toutes les touches du piano + les options relatives au piano.
+ * \author Dennis Bordet.
+ */
 #ifndef PIANO_H
 #define PIANO_H
 
@@ -7,28 +13,60 @@
 #include <QMessageBox>
 #include <QVector>
 
-
+/**
+ * \brief Comprend toutes les touches du piano + les options relatives au piano.
+ */
 class Piano : public QWidget
 {
     Q_OBJECT
 public:
+    /**
+     * \brief constructeur qui initialise tous les boutons de la partie basse du programme.
+     */
     explicit Piano(QWidget *parent = 0);
+    /**
+     * \brief tableau des notes jouées par l'utilisateur.
+     */
     QVector<QString> *notes_jouees;
+    /**
+     * \brief fonction comparant le tableau des notes jouées avec un autre passé en paramètre.
+     * Concrétement, le tableau passé en paramètre est celui contenant les notes de la partition.
+     */
     QVector<bool>* comparaison(QVector<QString> *partition);
     int scoreMsg();
     bool showing;
 
 signals:
+  /**
+   * \brief signal envoyé a chaque note jouée.
+   * il est envoyé à la partition afin de mettre à jour le curseur.
+   */
     void played();
 
 private:
+  /**
+   * \brief tableau des touches, facilite certains traitement groupés.
+   */
     QVector<Touche*> *notes;
+    /**
+     * \brief booleen indiquant si les raccourcis sont affichés sur les touches.
+     */
     bool b_racc_disp;
+    /**
+     * \brief booleen indiquant si les noms de notes sont affichés sur les touches.
+     */
     bool b_names_dip;
     int nbOfSuccess;
 
 public slots:
+  /**
+   * \brief permet d'enlever la dernière note jouée de son tableau et reculer le curseur.
+   */
     void retour_arriere();
+    
+    /**
+     * \brief joue un do aigu
+     */
     void play_dom();
     void play_rem();
     void play_mim();
@@ -37,6 +75,9 @@ public slots:
     void play_lam();
     void play_sim();
 
+    /**
+     * \brief joue un do grave
+     */
     void play_doM();
     void play_reM();
     void play_miM();
@@ -58,8 +99,18 @@ public slots:
     void play_faMD();
     void play_solMD();
     void play_laMD();
+    
+    /**
+     * \brief affiche les raccourcis clavier sur le piano.
+     */
     void display_racc();
+    /**
+     * \brief affiche les notes clavier sur le piano.
+     */
     void display_names();
+    /**
+     * \brief affiche/masque les options du piano et le redimensionne en consequence.
+     */
     void checking(bool checked);
 
 };
--- quelleEstCetteNote/QuelleEstCetteNote2/touche.h
+++ quelleEstCetteNote/QuelleEstCetteNote2/touche.h
@@ -1,20 +1,48 @@
+/**
+ * \file touche.h
+ * \brief contient la Classe Touche qui correspond à une touche du piano.
+ * \author Dennis Bordet.
+ */
 #ifndef TOUCHE_H
 #define TOUCHE_H
 #include <QPushButton>
 #include <QString>
 
-
+/**
+ * \class Touche.
+ * \brief Hérite de QPushButton, représente une touche de piano.
+ */
 class Touche :/*public  QWidget,*/ public QPushButton
 {
 public:
+    /**
+     * \brief constructeur hérité de QPushbutton qui initialise le racourci clavier et le nom sa note associée.
+     * Construit une touche de piano (taille, raccourci, nom)
+     */
     Touche(QWidget *parent, QString  raccourci=QString("rac"), QString note=QString("note"));
+    /*
+     * \brief getter donnant le nom de la note associé à la touche.
+     */
     QString getNote();
+    /**
+     * \brief getter donnant le raccourci de la touche.
+     */
     QString getRacc();
     //bool hitButton(const QPoint & pos) const;
+    
+    /**
+     * \brief transforme une note blanche (d'origine) en note noire.
+     * Redimensionne et colorie différement.
+     * change l'attribut blacked.
+     */
     void black();
+    /**
+     * \brief booleen indiquant si une note est noire ou non.
+     */
     bool blacked;
 
 private:
+  
     QString racc;
     QString note;
 
